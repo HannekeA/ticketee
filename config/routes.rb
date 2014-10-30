fe42788c00e5c62fd5922acfe784a8c7bd53217c
Hanneke van den Akker <hanneke@orikami.nl>
2014-10-30 14:51:45 +0100

    Implemented sign up.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Ticketee::Application.routes.draw do
+ 
+
   root "projects#index"
   
   resources :projects do
     resources :tickets
   end
+  resources :users
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
